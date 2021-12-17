037a7bc6a7ba0798c8c4dfbc0ba90bd58dffa744
robcabobb <robcabobb@gmail.com>
2021-12-17 17:57:21 -0500

    added js to filter by password settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,52 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// DOM elements
+const passwordEl = document.getElementById('password');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateBtnEl = document.getElementById("generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
 
-  passwordText.value = password;
+// Add event listener to generate button
+generateBtnEl.addEventListener("click", () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
 
+
+    passwordEl.innerText = generatePassword(
+        hasLower,
+        hasUpper, 
+        hasNumber, 
+        hasSymbol, 
+        length
+    );
+});
+
+// Generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+
+    let generatedPassword = '';
+
+    const typesCount = lower + upper + number + symbol;
+
+    console.log('typesCount: ', typesCount);
+
+    const typesArr = [{lower}, {upper}, {number}, {symbol}];
+
+    console.log('typesArr: ', typesArr);
 }
+
+// Generator functions
 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -27,14 +65,5 @@ function getRandomSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-const passwordEl = document.getElementById('password')
-const lengthEl = document.getElementById('length')
-const uppercaseEl = document.getElementById('uppercase')
-const lowercaseEl = document.getElementById('lowercase')
-const numbersEl = document.getElementById("numbers")
-const symbolsEl = document.getElementById("symbols")
-const generateEl = document.getElementById("generate")
-const resultEl = document.getElementById("password")
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
