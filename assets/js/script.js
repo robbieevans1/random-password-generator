d362adc97919adb9c4c9d8fb672818c28dab1a89
robcabobb <robcabobb@gmail.com>
2021-12-17 17:05:19 -0500

    added js to retrieve random numbers and symbols

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,31 @@ function writePassword() {
 
 }
 
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*()[]{}=/?';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+const passwordEl = document.getElementById('password')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById("numbers")
+const symbolsEl = document.getElementById("symbols")
+const generateEl = document.getElementById("generate")
+const resultEl = document.getElementById("password")
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-const resultEl = document.getElementById("password")
-const lengthEl = document.getElementById("password")
-const resultEl = document.getElementById("password")
-const resultEl = document.getElementById("password")
-const resultEl = document.getElementById("password")
-const resultEl = document.getElementById("password")
-const resultEl = document.getElementById("password")
-const resultEl = document.getElementById("password")
